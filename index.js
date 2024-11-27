91e9360f846c7f2edb3e71d2aff48abf3d432ba2
migu31-h <miguel196915.maha@gmail.com>
2024-11-27 11:36:42 -0500

    Connecting from Node.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-import express from 'express'; 
+import "dotenv/config"
+import express from 'express';
+import mongoose from "mongoose";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
@@ -8,6 +10,9 @@ import "dotenv/config";
 import session from 'express-session';
 import ModuleRoutes from './Kanbas/Modules/routes.js';
 import AssignmentRoutes from './Kanbas/Assignments/routes.js';
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
+
 const app = express();
 app.use(
     cors({
